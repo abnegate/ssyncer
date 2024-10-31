7a81782e6ff9af3f953acfc9ad3d774c07e95d45
Jake Barnby <jakeb994@gmail.com>
2024-11-01 12:37:45 +1300

    Eager load JSONAppStorage

--- Sshyncer/Sshyncer/Extensions/JSONAppStorage.swift
+++ Sshyncer/Sshyncer/Extensions/JSONAppStorage.swift
@@ -10,14 +10,24 @@ import Foundation
 @propertyWrapper
 struct JSONAppStorage<T: Codable> {
     let key: String
-    let defaultValue: T? = nil
+    let defaultValue: T?
     var storage: UserDefaults = .standard
+    
+    private var value: T?
+    
+    init(key: String, defaultValue: T? = nil, storage: UserDefaults = .standard) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.storage = storage
+        self.value = storage.getObject(T.self, forKey: key) ?? defaultValue
+    }
     
     var wrappedValue: T? {
         get {
             return storage.getObject(T.self, forKey: key) ?? defaultValue
         }
         set {
+            value = newValue
             storage.setObject(newValue, forKey: key)
         }
     }
